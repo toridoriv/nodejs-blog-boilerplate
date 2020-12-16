2bf9ca2a6eb72b151e5b500cbebedf37d08ccb09
Victoria Rodriguez <vrodriguezfe@icloud.com>
2020-12-16 02:28:21 -0300

    First Commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const articleRoute = require('./routes/articles');
+const Article = require('./models/article');
+const methodOverride = require('method-override');
+
+mongoose.connect('mongodb://localhost/blog', {
+	useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+});
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use('/articles', articleRoute);
+
+ app.get('/', async (req, res) => {
+
+	 const articles = await Article.find().sort({
+		 createdAt: 'desc'
+	 });
+
+	 res.render('articles/index', { articles: articles });
+ });
+
+app.listen(3000);
